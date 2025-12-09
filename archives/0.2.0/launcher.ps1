b511dc936b77ea02e890f272de776a3022d15c85
Mayonnaisu <116666368+Mayonnaisu@users.noreply.github.com>
2025-12-10 06:52:56 +0700

    Stop tracking archives

--- archives/0.2.0/launcher.ps1
+++
@@ -1,9 +0,0 @@
-Write-Host "PowerShell $((Get-Host).Version.ToString())"
-
-venv/Scripts/activate
-
-python main.py --input ".\test\local\New folder"
-
-# Show exit confirmation
-Write-Host "`nPress Enter to exit" -ForegroundColor Cyan -NoNewLine
-Read-Host
