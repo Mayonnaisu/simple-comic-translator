46e128037af02ec704cfc8c6bb381878a1875685
Mayonnaisu <116666368+Mayonnaisu@users.noreply.github.com>
2025-12-22 22:45:39 +0700

    Add logging

--- app/core/model.py
+++ app/core/model.py
@@ -1,11 +1,12 @@
 import os
 from tqdm import tqdm
+from loguru import logger
 from huggingface_hub import hf_hub_download, snapshot_download
 
 os.environ["HF_HUB_DISABLE_PROGRESS_BARS"] = "0"
 
 def download_model(repo_id, file_name, local_dir):
-    print("\nFile not exist. Initiating download.")
+    logger.info("\nModel does not exist. Initiating download.")
 
     local_path = hf_hub_download(
         repo_id=repo_id,
@@ -13,14 +14,14 @@ def download_model(repo_id, file_name, local_dir):
         local_dir=local_dir
     )
 
-    print(f"File downloaded to: {local_path}")
+    logger.info(f"Model downloaded to: {local_path}")
 
 def download_repo_snapshot(repo_id, local_dir):
-    print("\nModel repo snapshot does not exist. Initiating download.")
+    logger.info("\nRepo snapshot does not exist. Initiating download.")
 
     local_path = snapshot_download(
         repo_id=repo_id,
         local_dir=local_dir
     )
 
-    print(f"Model repo snapshot downloaded to: {local_path}")
+    logger.info(f"Repo snapshot downloaded to: {local_path}")
