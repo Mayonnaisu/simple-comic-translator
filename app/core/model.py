9a6dbeaf5086cdfe6071e3f249f93c45bca24ac1
Mayonnaisu <116666368+Mayonnaisu@users.noreply.github.com>
2025-12-22 22:59:55 +0700

    Add color

--- app/core/model.py
+++ app/core/model.py
@@ -1,12 +1,14 @@
 import os
 from tqdm import tqdm
 from loguru import logger
+from colorama import Fore, Style, init
 from huggingface_hub import hf_hub_download, snapshot_download
 
 os.environ["HF_HUB_DISABLE_PROGRESS_BARS"] = "0"
+init(autoreset=True)
 
 def download_model(repo_id, file_name, local_dir):
-    logger.info("\nModel does not exist. Initiating download.")
+    logger.info(Style.BRIGHT + Fore.YELLOW + "\nModel does not exist. Initiating download...")
 
     local_path = hf_hub_download(
         repo_id=repo_id,
@@ -14,14 +16,14 @@ def download_model(repo_id, file_name, local_dir):
         local_dir=local_dir
     )
 
-    logger.info(f"Model downloaded to: {local_path}")
+    logger.info(Style.BRIGHT + Fore.GREEN + f"Model downloaded to: {local_path}")
 
 def download_repo_snapshot(repo_id, local_dir):
-    logger.info("\nRepo snapshot does not exist. Initiating download.")
+    logger.info(Style.BRIGHT + Fore.YELLOW + "\nRepo snapshot does not exist. Initiating download...")
 
     local_path = snapshot_download(
         repo_id=repo_id,
         local_dir=local_dir
     )
 
-    logger.info(f"Repo snapshot downloaded to: {local_path}")
+    logger.info(Style.BRIGHT + Fore.GREEN + f"Repo snapshot downloaded to: {local_path}")
